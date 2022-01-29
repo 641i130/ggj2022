7f53b6fb8842229163805028d2ca2b7139d7c941
Caret <c4r3t@yubi.moe>
2022-01-28 18:09:31 -0600

    Working on gen

--- src/World.gd
+++ src/World.gd
@@ -9,28 +9,85 @@ var unready_chunks = {}
 var thread
 
 func _ready():
+	# Init randomization stuffs
 	randomize()
 	noise = OpenSimplexNoise.new()
 	noise.seed = randi()
 	noise.period = 80
 	noise.octaves = 6
-	
+	# Enable threading for faster loadtimes!
 	thread = Thread.new()
 
 func add_chunk(x, z):
-	var key = str(x) + "," + str(z)
-	if chunks.has(key) or unready_chunks.has(key):
+	var key = str(x) + "," + str(z) # Yes, keys are strings :P TODO optimize
+	if chunks.has(key) or unready_chunks.has(key): # If the key we're making exsists, don't generate!
 		return
 	
-	if not thread.is_active():
+	if not thread.is_active(): # If the thread didn't start, start it
 		thread.start(self, "load_chunk",[thread,x,z])
-		unready_chunks[key] = 1
+		unready_chunks[key] = 1 # Sets the chunk to unready state so we can add to it
 
 func load_chunk(arr):
 	var thread = arr[0]
 	var x = arr[1]
 	var z = arr[2]
-	var chunk = Chunk.new(noise, x*chunk_size, z*chunk_size, chunk_size)
+	var chunk = Chunk.new(noise, x*chunk_size, z*chunk_size, chunk_size) # Gen chunk
+	chunk.translation = Vector3(x * chunk_size, 0, z * chunk_size) # Set chunk position
+	
+	call_deferred("load_done", chunk, thread) # End chunk gen for chunk
+	
+func load_done(chunk, thread):
+	add_child(chunk)
+	var key = str(chunk.x / chunk_size) + "," + str(chunk.z / chunk_size) # TODO optimize
+	chunks[key] = chunk #PUT into ready chunks!
+	unready_chunks.erase(key) # remove from ready state buffer
+	thread.wait_to_finish()
+	
+func get_chunk(x, z):
+	var key = str(x) + "," + str(z)
+	if chunks.has(key):
+		return chunks.get(key)
+	return null
+	
+func _process(delta):
+	update_chunks() 
+	clean_up_chunks()
+	reset_chunks()
+	
+func update_chunks():
+	# Get player location
+	var player_translation = $Player.translation
+	var p_x = int(player_translation.x)/chunk_size
+	var p_z = int(player_translation.z)/chunk_size
+	
+	for x in range(p_x - chunk_amount * 0.5, p_x + chunk_amount * 0.5):
+		for z in range(p_z - chunk_amount * 0.5, p_z + chunk_amount * 0.5):
+			add_chunk(x,z)
+	
+	
+func clean_up_chunks():
+	pass
+	
+func reset_chunks():
+	pass
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
--- src/chunk.gd
+++ src/chunk.gd
@@ -17,24 +17,32 @@ func _ready():
 	generate_chunk()
 	
 func generate_chunk():
+	
 	var noise = OpenSimplexNoise.new()
 	noise.period = 80
 	noise.octaves = 6
-	var plane_mesh = PlaneMesh.new()
+
+	var plane_mesh = PlaneMesh.new() # Base mesh we are modifying
 	plane_mesh.size = Vector2(400,400)
 	plane_mesh.subdivide_depth = 100
 	plane_mesh.subdivide_width = 100
+	
 	var surface_tool = SurfaceTool.new()
 	surface_tool.create_from(plane_mesh, 0)
+	
 	var array_plane = surface_tool.commit()
+	
 	var data_tool = MeshDataTool.new()
 	data_tool.create_from_surface(array_plane,0)
+	
 	for i in range(data_tool.get_vertex_count()):
 		var vertex = data_tool.get_vertex(i)
-		vertex.y = noise.get_noise_3d(vertex.x,vertex.y,vertex.z) * 60
+		vertex.y = noise.get_noise_3d(vertex.x,vertex.y,vertex.z) * 60 # Randomly set y values to make spiky
 		data_tool.set_vertex(i,vertex)
+		
 	for i in range(array_plane.get_surface_count()):
 		array_plane.surface_remove(i)
+		
 	data_tool.commit_to_surface(array_plane)
 	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
 	surface_tool.create_from(array_plane,0)
