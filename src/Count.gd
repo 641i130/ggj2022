81c3f7d6db80bcb666af37dab02ef0983b71acd1
641i130 <galileoray@gmail.com>
2022-01-30 01:21:36 -0600

    counter works

---
+++ src/Count.gd
@@ -0,0 +1,9 @@
+extends RichTextLabel
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.text = "Rings : " + str(Player.points)
