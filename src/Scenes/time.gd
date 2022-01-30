43189791e2bff4e8efee183e3b06cb2259a70112
DeAngelo Wilson <deangelo76er@sbcglobal.net>
2022-01-30 03:34:13 -0600

    added night cycle maths and timing things

--- src/Scenes/time.gd
+++ src/Scenes/time.gd
@@ -1,10 +1,43 @@
 extends DirectionalLight3D
 
+# rotation angle in radians
+const cycle_rot_start = 0
+const cycle_rot_end = -3.14/2
+
+#debug
+const max_tic_rate = 0.04
+const min_tic_rate = 0.0001 # smooth day transition delta (30secs-1min cycle)
+	
+var current_time 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-	self.rotation = Vector3(-5,180,180)
+	#pass # Replace with function body.
+	self.initialize()
+	
+func initialize():
+	self.rotation = Vector3(cycle_rot_start,180,180)
+	self.current_time = 0.0
+	print("init")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.rotation.x += 0.04
+	#self.rotation.x += min_tic_rate
+	#print(self.rotation.x)
+	#print(delta)
+	
+	self.current_time += delta
+	var max_time = Player.getTotalTime()
+	var tS = (current_time / max_time)
+
+	self.quaternion = self.quaternion.slerp(Quaternion(Vector3.RIGHT, cycle_rot_end),tS * delta)
+
+	if current_time >= max_time:
+		self.CycleComplete()
+
+##
+## TODO:: handle cycle complete => switch scene || end game
+##
+func CycleComplete():
+	self.initialize()
+	Player.CycleComplete(Player.Mode.NIGHT)
--- src/Scripts/Player.gd
+++ src/Scripts/Player.gd
@@ -1,19 +1,45 @@
 extends Node
 
+enum Mode {MENU, NIGHT, DAY}
+
+const ringTimeValue_night = 1.0
+const ringTimeValue_day = 0.5
 
 var points
+var day_cycle_total_time
 
+var mode 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.points = 0
-
+	self.mode = Mode.NIGHT
+	self.day_cycle_total_time = 30 # player gets atleast 10secs in day cycle
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+#may require knowing which cycle finsihed if reused
+func CycleComplete(mode):
+	if mode == Mode.DAY:
+		GameOver()
 
+func GameOver():
+	pass
+	
+func getTotalTime():
+	return day_cycle_total_time
+
+func addTime(delta):
+	self.day_cycle_total_time += delta
+	
 func ringCollected():
-	points += 1
-	print(points)
+	#TODO:: remove conditional by creating 'day'+'night' rings
+	if mode == Mode.NIGHT:
+		self.addTime(ringTimeValue_night)
+	if mode == Mode.DAY:
+		self.addTime(ringTimeValue_day)
+		points += 1
+
 	
 	
