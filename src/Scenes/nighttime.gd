b4d57f49dcce89197e57f77538d5309cda7b0004
DeAngelo Wilson <deangelo76er@sbcglobal.net>
2022-01-30 09:34:06 -0600

    cleaned nightime=>day time cycling (cycle.gb common)

--- src/Scenes/nighttime.gd
+++ src/Scenes/nighttime.gd
@@ -6,15 +6,13 @@ const cycle_rot_end = -3.14/2
 
 var cycle
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#pass # Replace with function body.
 	self.initialize()
 
 func initialize():
 	self.rotation = Vector3(self.cycle_rot_start,180,180)
-	self.cycle = Cycle.new(Player.Mode.DAY)
+	self.cycle = Cycle.new(Player.Mode.NIGHT)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -25,8 +23,6 @@ func _process(delta):
 	if self.cycle.isCycleComplete():
 		self.CycleComplete()
 
-## ********************************************************
-## TODO:: handle cycle complete => end game...
-## ********************************************************
 func CycleComplete():
-	self.initialize()
+	get_tree().change_scene("res://Scenes/Day.tscn")	
+
--- src/Scripts/cycle.gd
+++ src/Scripts/cycle.gd
@@ -1,39 +1,42 @@
-#extends Node3D
-#class_name Cycle
-#
-#
-## Called when the node enters the scene tree for the first time.
-#func _init():
-#	#self.mode = _mode
-#	# Just store curret gameMode in Player... (can be used across scenes)
-#
-#	self.current_time = 0.0
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func update(delta):
-#	#self.rotation.x += min_tic_rate
-#	#print(self.rotation.x)
-#	#print(delta)
-#
-#
-#func isCycleComplete():
-#	if current_time >= getTotalTime():
-#		return true
-#	return false
-#
-#
+extends Node3D
+class_name Cycle
+
+var current_time 
+
+# Called when the node enters the scene tree for the first time.
+func _init(_mode):
+	Player.SetMode(_mode)
+	# Just store curret gameMode in Player... (can be used across scenes)
+	self.current_time = 0.0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func update(delta):
+	#self.rotation.x += min_tic_rate
+	#print(self.rotation.x)
+	#print(delta)
+	self.current_time += delta
+	var max_time = Player.getCycleTotalTime()
+	return (current_time / max_time)
+
+
+func isCycleComplete():
+	if current_time >= Player.getCycleTotalTime():
+		return true
+	return false
+
+
 #func getTotalTime():
 #	Player.getCycleTotalTime()
-##	if self.mode == Player.Mode.NIGHT:
-##		return Player.getNightCycleTotalTime()
-##	elif self.mode == Player.Mode.DAY:
-##		return Player.getDayCycleTotalTime()
-#
-#
+#	if self.mode == Player.Mode.NIGHT:
+#		return Player.getNightCycleTotalTime()
+#	elif self.mode == Player.Mode.DAY:
+#		return Player.getDayCycleTotalTime()
+
+
+##
+## TODO:: handle cycle complete => switch scene || end game
 ###
-### TODO:: handle cycle complete => switch scene || end game
-####
-##func CycleComplete():
-##	self.initialize()
-##	Player.CycleComplete()
+#func CycleComplete():
+#	self.initialize()
+#	Player.CycleComplete()
